164ec39951e72d892fb2b1cd050afd7109049181
Fatemeh <fatemehfaryadras7@gmail.com>
2023-04-25 12:47:41 +0330

    third commit

--- content/js/script.js
+++ content/js/script.js
@@ -10,6 +10,8 @@ var fruit = [
 let min = 0;
 let max = 6;
 let answer = '';
+let wordEl = null;
+text = [];
 
 
 
@@ -17,12 +19,12 @@ function Randowm() {
       answer = fruit[Math.floor(Math.random() * 6)];
 }
 
-function keyboard(){
+function keyboard() {
     let btnKeyboard ="qwertyuiopasdfghjklzxcvbnm".split('').map(e =>
         `
         <button class="btnEl bg-pink-500 m-1 w-12 h-12 rounded-lg"
         id='` + e + `'
-        onClick="keyEl('` + e + `')"
+        onClick="handeling('` + e + `')"
         >
         ` + e + `
         </button>
@@ -32,4 +34,32 @@ function keyboard(){
     document.querySelector("#keyboard").innerHTML = btnKeyboard;
 }
 
+function handeling(){
+     
+}
+
+function picture() {
+    document.querySelector(".img").src = "./content/img" + min +".jpg"
+}
+
+function Won() {
+    if(wordEl == answer){
+        document.querySelector("#keyboard").innerHTML = "You Won !";
+    }
+}
+
+function Lost() {
+     if(min == max){
+        document.querySelector("#word").innerHTML = "The Answer Was: " + answer
+        document.querySelector("#keyboard").innerHTML = "You Lost !";
+     }
+}
+
+function reset() {
+    text = [];
+   document.querySelector(".img").src = "./content/img/0.jpg"
+}
+
+
 keyboard()
+Randowm()
